6980d95357492533f63bb02fbf7e3542936a394d
Karo <44981312+KaroMourad@users.noreply.github.com>
2021-06-24 17:09:00 +0400

    Add ability to export chart as JPEG or PNG formats  image

--- aim/web/ui/src/screens/hub/HubMainScreen/components/ControlsSidebar/components/ControlsSidebarExport/ControlsSidebarExport.js
+++ aim/web/ui/src/screens/hub/HubMainScreen/components/ControlsSidebar/components/ControlsSidebarExport/ControlsSidebarExport.js
@@ -1,88 +1,134 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
 import UI from '../../../../../../../ui';
 import { classNames } from '../../../../../../../utils';
 import './ControlsSidebarExport.less';
 
 import PropTypes from 'prop-types';
-import * as analytics from '../../../../../../../services/analytics';
 import html2canvas from 'html2canvas';
+import * as analytics from '../../../../../../../services/analytics';
+
+import { Instagram as ContentLoader } from 'react-content-loader';
+import moment from 'moment';
 
 function ControlsSidebarExport(props) {
-  let [opened, setOpened] = useState(false);
-  let [exportImgSrc, setExportImgSrc] = useState(null);
-  let [processing, setProcessing] = useState(false);
+  const [opened, setOpened] = useState(false);
+  const [exportImgCanvas, setExportImgCanvas] = useState(null);
+  const [exportImgMimeType, setExportImgMimeType] = useState('jpeg');
+  const [processing, setProcessing] = useState(false);
 
-  const clearLinesCircles = useCallback((svgElement) => {
-    const clonedSvgElement = svgElement.cloneNode(true);
-    const hoverLine = clonedSvgElement.querySelector('.HoverLine');
-    const hoverCircles = clonedSvgElement.querySelectorAll('.HoverCircle');
-    const lines = clonedSvgElement.querySelector('.Lines');
+  const imgBase64Src = useRef('');
+  const wrapper = useRef(null);
+  const timeOutId = useRef(null);
+
+  function closeModal() {
+    window.clearTimeout(timeOutId.current);
+    imgBase64Src.current = '';
+    wrapper.current?.remove();
+    setOpened(false);
+    setExportImgCanvas(null);
+  }
+
+  async function setImg() {
+    try {
+      const canvas = await html2canvas(wrapper.current);
+      imgBase64Src.current = canvas.toDataURL(
+        `image/${exportImgMimeType}`,
+        1.0,
+      );
+      setExportImgCanvas(canvas);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setProcessing(false);
+    }
+  }
+
+  const clearImage = useCallback((element) => {
+    const clonedElement = element.cloneNode(true);
+    const hoverLine = clonedElement.querySelector('.HoverLine');
+    const circles = clonedElement.getElementsByTagName('circle');
+    const lines = clonedElement.querySelector('.Lines');
     if (hoverLine) {
       hoverLine.remove?.();
     }
-    if (hoverCircles.length > 0) {
-      hoverCircles.forEach((hoverCircle) => {
-        hoverCircle.remove?.();
+    if (circles?.length > 0) {
+      Array.from(circles).forEach((circle) => {
+        circle.remove?.();
       });
     }
     if (lines) {
       lines.classList.remove('highlight');
     }
-    return clonedSvgElement;
+    return clonedElement;
   }, []);
+
+  const loadImg = useCallback(() => {
+    const panel = document.querySelector('.Panel');
+    if (panel) {
+      setProcessing(true);
+      const svgElements = panel.querySelectorAll('svg');
+      wrapper.current = document.createElement('div');
+      svgElements?.forEach((svgElement) => {
+        const clearedSvgElement = clearImage(svgElement);
+        wrapper.current.appendChild(clearedSvgElement);
+      });
+
+      const root = document.getElementById('root');
+      wrapper.current.style.position = 'fixed';
+      wrapper.current.style.zIndex = '-1';
+      wrapper.current.style.background = 'white';
+      wrapper.current.style.display = 'inline-flex';
+      wrapper.current.style.flexWrap = 'wrap';
+      wrapper.current.style.width = `${panel.offsetWidth + 10}px`;
+      wrapper.current.style.height = `${panel.offsetHeight + 10}px`;
+      root.appendChild(wrapper.current);
+
+      window.clearTimeout(timeOutId.current);
+      timeOutId.current = window.setTimeout(setImg);
+    }
+  }, [clearImage, setImg, setProcessing]);
+
+  const toggleModal = useCallback(() => {
+    setOpened((prevOpened) => {
+      if (prevOpened) {
+        closeModal();
+      }
+      return !prevOpened;
+    });
+  }, [closeModal]);
+
+  const downloadBase64File = useCallback((base64Data, fileName) => {
+    const downloadLink = document.createElement('a');
+    downloadLink.href = base64Data;
+    downloadLink.download = fileName;
+    downloadLink.click();
+  }, []);
+
+  const exportImgHandler = useCallback(
+    (imgCanvas, imgMimeType) => () => {
+      if (imgCanvas && imgMimeType) {
+        imgBase64Src.current = imgCanvas.toDataURL(`image/${imgMimeType}`, 1.0);
+        const filename = `chart-${moment().format(
+          'HH:mm:ss · D MMM, YY',
+        )}.${imgMimeType}`;
+        downloadBase64File(imgBase64Src.current, filename);
+        analytics.trackEvent('[Explore] Export chart image');
+      }
+    },
+    [downloadBase64File],
+  );
 
   useEffect(() => {
     if (opened) {
-      const panel = document.querySelector('.Panel');
-      if (panel) {
-        const svgElements = panel.querySelectorAll('svg');
-
-        if (svgElements) {
-          const wrapper = document.createElement('div');
-          svgElements.forEach((svgElement) => {
-            // const { width, height } = svgElement.getBoundingClientRect();
-            const clearedSvg = clearLinesCircles(svgElement);
-            wrapper.appendChild(clearedSvg);
-          });
-
-          const body = document.getElementsByTagName('body')[0];
-          body.appendChild(wrapper);
-          html2canvas(wrapper).then(function (canvas) {
-            const imgBase64 = canvas.toDataURL('image/png');
-            console.log('imgBase64:', imgBase64);
-            setExportImgSrc(imgBase64);
-          });
-        }
-      }
+      loadImg();
     }
   }, [opened]);
-
-  function closeModal() {
-    setOpened(false);
-    setExportImgSrc(null);
-  }
-
-  function toggleModal() {
-    setOpened((prevState) => !prevState);
-  }
-
-  function exportImgHandler() {
-    return new Promise((resolve, reject) => {
-      if (exportImgSrc) {
-        setProcessing(true);
-      }
-    });
-  }
-
-  function downloadImage(href, name) {
-    let link = document.createElement('a');
-    link.download = name;
-    link.style.opacity = '0';
-    document.body.append(link);
-    link.href = href;
-    link.click();
-    link.remove();
-  }
 
   return (
     <>
@@ -120,38 +166,43 @@ function ControlsSidebarExport(props) {
               </UI.Text>
             </div>
             <div className='Modal__body'>
-              <img src={exportImgSrc} width='100%' alt='' />
-              {/*<form onSubmit={save}>*/}
-              {/*  <div className='SaveAsModal__body__formInput'>*/}
-              {/*    <UI.Input*/}
-              {/*      onChange={(evt) => setName(evt.target.value)}*/}
-              {/*      name='name'*/}
-              {/*      value={name}*/}
-              {/*      placeholder={'Bookmark Name'}*/}
-              {/*      size='small'*/}
-              {/*      autoFocus*/}
-              {/*      autoComplete='off'*/}
-              {/*    />*/}
-              {/*  </div>*/}
-              {/*  <div className='SaveAsModal__body__formInput'>*/}
-              {/*    <UI.Input*/}
-              {/*      onChange={(evt) => setDesc(evt.target.value)}*/}
-              {/*      name='desc'*/}
-              {/*      value={desc}*/}
-              {/*      placeholder={'Bookmark Description'}*/}
-              {/*      size='small'*/}
-              {/*      autoComplete='off'*/}
-              {/*    />*/}
-              {/*  </div>*/}
-              {/*</form>*/}
+              {processing ? (
+                <ContentLoader
+                  width={100}
+                  height={'250px'}
+                  viewBox='0 0 400 200'
+                  style={{ width: '100%' }}
+                />
+              ) : (
+                <img
+                  src={imgBase64Src.current}
+                  width='100%'
+                  height='250px'
+                  alt='exported image'
+                  style={{ objectFit: 'contain' }}
+                />
+              )}
             </div>
             <div className='Modal__body__formFooter'>
+              <UI.Dropdown
+                width={150}
+                options={['jpeg', 'png'].map((val) => ({
+                  value: val,
+                  label: `${val.toUpperCase()}`,
+                }))}
+                defaultValue={{
+                  value: exportImgMimeType,
+                  label: `${exportImgMimeType.toUpperCase()}`,
+                }}
+                onChange={(option) => setExportImgMimeType(option.value)}
+              />
               <UI.Button
                 className='Modal__body__formFooter__button'
                 type='primary'
                 size='tiny'
-                onClick={exportImgHandler}
-                disabled={processing || !exportImgSrc}
+                style={{ marginLeft: 'auto' }}
+                onClick={exportImgHandler(exportImgCanvas, exportImgMimeType)}
+                disabled={processing || !imgBase64Src.current}
               >
                 Export
               </UI.Button>
